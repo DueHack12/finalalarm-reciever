9b08b52ff465239f35ec1020a66643d94b0aee0c
Jack Singewald <63979352+DueHack12@users.noreply.github.com>
2024-04-24 07:53:42 -0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 def on_received_string(receivedString):
     basic.show_string(receivedString)
     if receivedString == "ALARM":
-        pass
+        music.play(music.string_playable("C5 D C5 D C5 D C5 D ", 120),
+            music.PlaybackMode.LOOPING_IN_BACKGROUND)
+    elif receivedString == "Alarm Cleared":
+        control.reset()
 radio.on_received_string(on_received_string)
 
-radio.set_group(1)
-
-music.play(music.string_playable("C5 D C5 D C5 D C5 D ", 120),
-            music.PlaybackMode.LOOPING_IN_BACKGROUND)
+radio.set_group(5)
+radio.set_frequency_band(37)
+basic.pause(1000)
+basic.show_string("Ready to Receive")
--- main.ts
+++ main.ts
@@ -1,9 +1,44 @@
+input.onButtonPressed(Button.AB, function () {
+    led2 = 0
+    basic.clearScreen()
+    music.stopAllSounds()
+    basic.showString("Cleared")
+    radio.sendNumber(65)
+    control.reset()
+})
 radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
     if (receivedString == "ALARM") {
+        basic.showString(receivedString)
         music.play(music.stringPlayable("C5 D C5 D C5 D C5 D ", 120), music.PlaybackMode.LoopingInBackground)
-    } else if (receivedString == "Alarm Cleared") {
+        led2 = 1
+        while (led2 == 1) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                . . . . .
+                `)
+        }
+    }
+    if (receivedString == "Alarm Cleared") {
+        led2 = 0
+        basic.clearScreen()
         music.stopAllSounds()
+        basic.showString("Cleared")
+        control.reset()
     }
 })
-radio.setGroup(1)
+let led2 = 0
+led2 = 0
+radio.setGroup(5)
+radio.setFrequencyBand(37)
+basic.pause(1000)
+basic.showString("RR")
